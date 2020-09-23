389f11fb22c27ada8ad9dcd929564287d4ae97a4
Tyler Long <tyler4long@gmail.com>
2020-09-23 12:32:36 -0700

    HI-DPI

--- src/main.ts
+++ src/main.ts
@@ -42,8 +42,9 @@ const main = () => {
 
   const resizeRendererToDisplaySize = (renderer: THREE.WebGLRenderer) => {
     const canvas = renderer.domElement;
-    const width = canvas.clientWidth;
-    const height = canvas.clientHeight;
+    const pixelRatio = window.devicePixelRatio;
+    const width = (canvas.clientWidth * pixelRatio) | 0;
+    const height = (canvas.clientHeight * pixelRatio) | 0;
     const needResize = canvas.width !== width || canvas.height !== height;
     if (needResize) {
       renderer.setSize(width, height, false);
